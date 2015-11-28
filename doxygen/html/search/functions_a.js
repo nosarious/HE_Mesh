e643ee41df89b1a536cff6589bb9461487517fa7
Frederik Vanhoutte (W:Blut) <fvanhoutte@wblut.com>
2015-11-28 21:38:16 +0100

    Removing docs

--- doxygen/html/search/functions_a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['jacobi',['Jacobi',['../classwblut_1_1math_1_1_w_b___m33.html#a398523e57a960736cfc68682b4517de8',1,'wblut::math::WB_M33']]],
-  ['joinholetoshell',['joinHoleToShell',['../classwblut_1_1geom_1_1_w_b___triangulate.html#a7ad76d44198f60762087e7eb05d0c6a8',1,'wblut::geom::WB_Triangulate']]]
-];
